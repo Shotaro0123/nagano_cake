223c2f2f08a8de6feaacd7f3ccff1324ca1afa79
EC2 Default User <ec2-user@ip-172-31-6-88.ap-northeast-1.compute.internal>
2023-05-29 05:03:44 +0000

    全体像から

--- db/migrate/20230424040721_devise_create_customers.rb
+++ db/migrate/20230424040721_devise_create_customers.rb
@@ -3,9 +3,6 @@
 class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
