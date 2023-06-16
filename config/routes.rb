6bfb3ffd8be25a977e31c04cada8e283eabbcc15
EC2 Default User <ec2-user@ip-172-31-6-88.ap-northeast-1.compute.internal>
2023-06-16 07:50:20 +0000

    ヘッダーの遷移先ができてるところまで管理者と顧客のログイン後の遷移先がわからない

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
 
   get '/' => 'public/homes#top'
   get 'about' => 'public/homes#about'
+  get '/items' => 'public/items#index'
+  get '/cart_items' => 'public/cart_items#index'
 
   get 'admin' => 'admin/homes#top'
   get 'admin/items' => 'admin/items#index'
   get 'admin/customers' => 'admin/customers#index'
+
+  get '/items' => 'public/items#index'
+  get '/customers/my_page' => 'public/customers#show'
 
   devise_for :customers,skip: [:passwords], controllers: {
    registrations: "public/registrations",
