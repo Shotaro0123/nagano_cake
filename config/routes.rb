0d3f37d96e7998813adbe1eda1fced80c2eafce9
EC2 Default User <ec2-user@ip-172-31-6-88.ap-northeast-1.compute.internal>
2023-06-22 04:53:47 +0000

    マイページの作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
 
  root to:"public/homes#top"
 
-  get '/' => 'public/homes#top'
+  get '' => 'public/homes#top'
   get 'about' => 'public/homes#about'
-  get '/items' => 'public/items#index'
-  get '/cart_items' => 'public/cart_items#index'
+  get 'items' => 'public/items#index'
+  get 'cart_items' => 'public/cart_items#index'
+  get 'customers/my_page' => 'public/customers#show'
 
-  get 'admin' => 'admin/homes#top'
-  get 'admin/items' => 'admin/items#index'
-  get 'admin/customers' => 'admin/customers#index'
-
-  get '/items' => 'public/items#index'
-  get '/customers/my_page' => 'public/customers#show'
-  resources :items, only:[:index]
+  #以下adminコントローラのルーティング
+  namespace :admin do
+  get '' => 'homes#top'
+  get 'items' => 'items#index'
+  get 'customers' => 'customers#index'
+ end
 
   devise_for :customers,skip: [:passwords], controllers: {
    registrations: "public/registrations",
