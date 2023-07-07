cd2437308884e8a7d6d510ddfe38f5a33608666c
EC2 Default User <ec2-user@ip-172-31-6-88.ap-northeast-1.compute.internal>
2023-07-07 09:45:50 +0000

    商品詳細まで

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,11 +10,14 @@ class Admin::ItemsController < ApplicationController
  def create
   @item = Item.new(item_params)
   @item.save
-  redirect_to admin_items_path
+  redirect_to admin_item_path(@item.id)
  end
 
  def show
   @item = Item.find(params[:id])
+ end
+
+ def edit
  end
 
  private
