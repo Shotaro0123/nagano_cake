b48bf058f55e56e58a34c4b4577f7939459e2919
EC2 Default User <ec2-user@ip-172-31-6-88.ap-northeast-1.compute.internal>
2023-08-07 04:21:04 +0000

    カートの削除機能とカートの数量変更がわからない

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,7 @@
 class CartItem < ApplicationRecord
  belongs_to :customer
- belongs_to :item
+ belongs_to :item, optional: true
+ def subtotal
+    item.with_tax_price * amount
+ end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
  #belongs_to :genre
  has_many :cart_items
  has_many :order_details
-  def add_tax_price
-   (self.price * 1.10).round
+  def with_tax_price
+   (price * 1.1).floor
   end
   def get_image
     if image.attached?
