7b96526d32397cbe91da1ecf96d93b61f3b77e61
EC2 Default User <ec2-user@ip-172-31-6-88.ap-northeast-1.compute.internal>
2024-01-16 06:51:56 +0000

    注文確定まで

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,15 +10,14 @@ class Public::OrdersController < ApplicationController
   @order = Order.new(orders_params)
   @order.customer_id = current_customer.id
   @order.save!
-  redirect_to　orders_complete_path
+  redirect_to１orders_complete_path
  end
 
  def confirm
   @order = Order.new(order_params)
   @order.postal_code = current_customer.postal_code
   @order.address = current_customer.address
-  @order.name = current_customer.last_name
-  @order.name = current_customer.first_name
+  @order.name = current_customer.last_name + current_customer.first_name
   @cart_items = current_customer.cart_items.all
  end
 
